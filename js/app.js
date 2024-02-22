856792384aaaee41a9a507692b67e524fa2c2b55
Hector <hectorfurini@outlook.com>
2024-02-22 12:58:33 -0300

    Atualizando app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-//Colher os dados de total dos três tipos de lugar
 let totalPista = document.getElementById('qtd-pista');
 let totalCadeiraSuperior = document.getElementById('qtd-superior');
 let totalCadeiraInferior = document.getElementById('qtd-inferior');
 
 function comprar() {
-    //Colher dados da quantidade e do tipo de lugar
     let tipoLugar = document.getElementById('tipo-ingresso').value;
     let quantidadePrompt = parseInt(document.getElementById('qtd').value);
 
-    //Tratar esses dados e diminuir do total
+    if (isNaN(quantidadePrompt)) {
+        return;
+    }
+    
     if (tipoLugar == 'inferior') {
         if (totalCadeiraInferior.textContent < quantidadePrompt) {
             alert('Quantidade de ingressos indisponivel para Cadeira Inferior!');
@@ -35,7 +36,6 @@ function comprar() {
 
     }
 
-    //limpar campo de quantidade
     quantidadePrompt = document.getElementById('qtd');
     quantidadePrompt.value = '';
 
